dc3686a000f6acba852ec58c5fa86e7b0da19133
Sravankumar1612 <sravankumartikkavarapu@gmail.com>
2023-07-26 15:57:47 +0530

    intial commit

---
+++ BankAccount.cs
@@ -0,0 +1,48 @@
+﻿using System;
+public class BankAccount
+{
+    private static int accountCounter = 1000;
+    public int AccountNumber { get; }
+    public string AccountHolderName { get; }
+    private double Balance { get; set; }
+
+    public BankAccount(string accountHolderName)
+    {
+        AccountNumber = GenerateAccountNumber();
+        AccountHolderName = accountHolderName;
+        Balance = 0;
+    }
+
+    private static int GenerateAccountNumber()
+    {
+        return ++accountCounter;
+    }
+
+    public void Deposit(double amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Deposit amount must be greater than zero.");
+        }
+
+        Balance += amount;
+    }
+    public void Withdraw(double amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Withdrawal amount must be greater than zero.");
+        }
+
+        if (amount > Balance)
+        {
+            throw new InvalidOperationException("Insufficient funds.");
+        }
+
+        Balance -= amount;
+    }
+    public double GetBalance()
+    {
+        return Balance;
+    }
+}
